faf961ec178f5e22fea04c670d260dc756b14b1c
phaeryl <florian.arnaud2019@gmail.com>
2024-04-09 15:31:48 +0200

    Random recipes display partially done

--- js/script.js
+++ js/script.js
@@ -48,6 +48,57 @@ function displayRecipes(recipes) {
   });
 }
 
+function generateRandomNumbers(recipes) {
+  const numbers = [];
+  let numberOfRecipes = recipes.length
+  while (numbers.length < 3) {
+    const randomNumber = Math.floor(Math.random() * numberOfRecipes);
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber);
+    }
+  }
+  return numbers;
+}
+
+function displayRandomRecipes(recipes) {
+  let numbers = generateRandomNumbers(recipes)
+  const container = document.querySelector("#container-random");
+  container.innerHTML = "";
+  for (let i = 0; i < numbers.length; i++) {
+    const ingredientsHtml = recipes[i].ingredients.map(
+      (ingredient) => `<li class="list-group-item">${ingredient.nom}: ${ingredient.quantite}</li>`
+    ).join("");
+
+    const stepsHtml = recipes[numbers[i]].etapes.map(
+      (step) => `<li class="list-group-item">${step}</li>`
+    ).join("");
+
+    const recipeHtml = `
+    <article class="col-sm-12 col-md-4">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <h3>${recipes[numbers[i]].nom}</h3>
+        </li>
+        <li class="list-group-item">
+          <h4>${recipes[numbers[i]].categorie}</h4>
+          <p><i class="fa-regular fa-clock"></i>${recipes[numbers[i]].temps_preparation}</p>
+        </li>
+        <li class="list-group-item">
+          <h4>Ingrédients</h4>
+          <ul class="list-group list-group-flush">${ingredientsHtml}</ul>
+        </li>
+        <li class="list-group-item">
+          <h4>Étapes</h4>
+          <ol class="list-group list-group-numbered list-group-flush">${stepsHtml}</ol>
+        </li>
+      </ul>
+    </article>`;
+
+    container.innerHTML += recipeHtml;
+
+  }
+
+}
 // Fonction pour filtrer les recettes
 function filterRecipes(recipes, searchTerm) {
   const lowerCaseSearchTerm = searchTerm.toLowerCase();
@@ -63,7 +114,8 @@ function filterRecipes(recipes, searchTerm) {
 fetchRecipes().then((recipes) => {
   // Accéder et afficher les recettes
   console.log(recipes);
-  displayRecipes(recipes);
+  console.log(generateRandomNumbers(recipes));
+  displayRandomRecipes(recipes)
 
   // Fonction de recherche
   const searchBar = document.getElementById("search");
